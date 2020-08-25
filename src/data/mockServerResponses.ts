b5ada1aaea21e13379ad75356d4eaf333be7c7b4
Felix Lopez <felix.lopez@vacasa.com>
2020-08-25 10:28:58 -0700

    Adds mock data and types.

---
+++ src/data/mockServerResponses.ts
@@ -0,0 +1,32 @@
+import { IQualificationResponseValues  } from '../types'
+
+export const qualifiedLoanData: IQualificationResponseValues = {
+  name: 'Car Loan prequalification resolution',
+  description: 
+  'Prequalification can help you determine whether you might be approved for a loan and an estimation of rates you might receive when shopping for a car.',
+  prequalification_status: 1,
+  resolution_message: 'Congratulations! Based on the information furnished by you, we are pleased to inform you that you pre-qualify for a car loan.',
+  applicationData: {
+    price: '',
+    make: '',
+    model: '',
+    income: '',
+    credit: ''
+  }
+}
+
+export const desqualifiedLoanData: IQualificationResponseValues = {
+  name: 'Car Loan prequalification resolution',
+  description: 
+  'Prequalification can help you determine whether you might be approved for a loan and an estimation of rates you might receive when shopping for a car.',
+  prequalification_status: 0,
+  resolution_message: 'We are sorry to inform you that your application for a car loan has been rejected',
+  applicationData: {
+    price: '',
+    make: '',
+    model: '',
+    income: '',
+    credit: ''
+  }
+}
+
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,21 @@
 export interface IAutoLoanFormValues {
-    price: '' | number,
+    price: number | '',
     make: string,
     model: string,
-    income: '' | number,
-    credit: '' | number
+    income: number | '',
+    credit: number | ''
 }
 
 export interface IRegistrationFormValues {
     email: string,
     password: string
     confirmPassword: string
+}
+
+export interface IQualificationResponseValues {
+    name: string,
+    description: string,
+    prequalification_status: number,
+    resolution_message: string,
+    applicationData: IAutoLoanFormValues
 }
