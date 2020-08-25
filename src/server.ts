5bb055694ab4a5f14812026de27308768676adfa
Felix Lopez <felix.lopez@vacasa.com>
2020-08-25 10:28:22 -0700

    Adds business rules and responses for both endpoints.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import { Server } from "miragejs"
+import { Server, Response } from "miragejs";
+import { desqualifiedLoanData, qualifiedLoanData } from './data/mockServerResponses';
 
 export function makeServer({ environment = "test" } = {}) {
   let server = new Server({
@@ -8,17 +9,17 @@ export function makeServer({ environment = "test" } = {}) {
       this.namespace = "api"
 
       this.get("/prequalified", (schema, request) => {
-        console.log(request.queryParams)
-        return {
-            response: 'Response from auto/prequalified'
-        }
+        const { price, credit, income } = request.queryParams;
+        // Some business rules.
+        // Since this is just a mock endpoint it doesn't include the appropiate backend validation.
+        if(parseInt(price) > 1000000) return new Response(400, { some: 'header' }, { errors: [ 'Car Price above threshold ($1,000,000).'] });
+        if( parseInt(price) > (parseInt(income) / 5) || parseInt(credit) < 600  ) return desqualifiedLoanData;
+        return {data: {...qualifiedLoanData, application_data: request.queryParams}};
       });
 
       this.post("/user", (schema, request) => {
         console.log(request.requestBody)
-        return {
-            response: 'Response from auto/user'
-        }
+        return {data: {message: 'Account created successfully!', status: 1}}
       });
     },
   })
