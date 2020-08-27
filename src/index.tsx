436c3861c2e3d3607d8897aff4234541e99725f8
Felix Lopez <felix.lopez@vacasa.com>
2020-08-27 16:16:05 -0700

    Adds production flag to allow Mirage to intercept http requests on production, for now.

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,9 @@ import { makeServer } from "./server"
 
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" })
+} else {
+  // Todo: Once we have a real server remove this flag.
+  makeServer({ environment: "production" })
 }
 
 ReactDOM.render(
